b6a5c6480efe983fa887cecb37bb19a3a427c1b9
thegouhund <alexandresapu1@gmail.com>
2024-09-19 17:25:11 +0800

    feat: add test cases

--- 2-selection-sort/Program.cs
+++ 2-selection-sort/Program.cs
@@ -1,4 +1,4 @@
-int[] selectionSort(int[] array)
+void selectionSort(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
@@ -8,18 +8,105 @@ int[] selectionSort(int[] array)
             if (array[min] > array[j]) min = j;
         }
 
-        int temp = array[i];
-        array[i] = array[min];
-        array[min] = temp;
+        // printArray(array);
+
+        if (array[i] != array[min])
+        {
+            // Console.WriteLine($"\nSwapping {array[i]} with {array[min]}");
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
     }
-
-    return array;
 }
 
-int[] unsortedArray = { 9, 2, 6, 0, 1, 3, 7, 8, 4 };
-int[] sortedArray = selectionSort(unsortedArray);
-
-for (int i = 0; i < sortedArray.Length; i++)
+void shuffle(int[] array)
 {
-    Console.Write(sortedArray[i] + " ");
+    Random rand = new();
+    int n = array.Length;
+    while (n > 1)
+    {
+        int k = rand.Next(n--);
+        int temp = array[n];
+        array[n] = array[k];
+        array[k] = temp;
+    }
 }
+
+void partialShuffle(int[] array, int swapAmount)
+{
+    Random rand = new Random();
+
+    for (int i = 0; i < swapAmount; i++)
+    {
+        int index1 = rand.Next(array.Length);
+        int index2 = rand.Next(array.Length);
+
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+}
+
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+void reverse(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[min] < array[j]) min = j;
+        }
+
+        if (array[i] != array[min])
+        {
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
+    }
+}
+
+
+int maxSize = 1000;
+
+int[] array = new int[maxSize];
+Random rand = new();
+for (int i = 1; i <= maxSize; i++)
+{
+    array[i - 1] = i;
+}
+
+// Test Cases
+
+// 1. Fully shuffled array
+shuffle(array);
+Console.WriteLine("Fully shuffled: ");
+printArray(array);
+selectionSort(array);
+Console.WriteLine("\n\nSorted array: ");
+printArray(array);
+
+// 2. Reversed array
+reverse(array);
+Console.WriteLine("\n\nReverse sorted: ");
+printArray(array);
+selectionSort(array);
+Console.WriteLine("\n\nSorted array: ");
+printArray(array);
+
+// 3. Paritally shuffled array
+partialShuffle(array, 5);
+Console.WriteLine("\n\nPartially shuffled: ");
+printArray(array);
+selectionSort(array);
+Console.WriteLine("\n\nSorted array: ");
+printArray(array);
